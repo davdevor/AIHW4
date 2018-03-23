c644f5c85b39df6874c29d282ffe4888f6434ee9
davdevor <drdcustomlows@gmail.com>
2018-03-23 13:12:31 -0400

    implmented methods for it to be able to run

--- src/AIHW4PycharmProject/GA.py
+++ src/AIHW4PycharmProject/GA.py
@@ -9,17 +9,22 @@ class GA:
         self.attributes = ['studytime','failures','paid','higher','freetime','absences','G1','G2','internet','schoolsup','G3']
         self.popSize = 200
         self.population = []
-        self.children = []
+        self.children = [0 for x in range(self.popSize)]
         self.fitness = []
         self.weights = 33;
-        self.ANN = ANN()
+        self.ANN = ANN.ANN()
         self.data = CsvReader.readCsv('../student-mat.csv',self.attributes)
         self.results = []
+        self.averageFitness = 0.0
 
     def computeFitness(self):
         for x in range(self.popSize):
-            fitness = float(self.results[x][1]-self.results[x][0])
+            t1 = int(self.results[x][1])
+            t2 = self.results[x][0]
+            fitness = float(t1-t2)
+            self.averageFitness += fitness
             self.fitness.append(fitness)
+        self.averageFitness /= self.popSize
 
     def offspring(self):
         prior = 0.0
@@ -40,19 +45,31 @@ class GA:
                 count += 1
                 r += 1.0
             else:
-                index += 1.0
+                index += 1
 
     def createNewPopulation(self):
         index = 0
         temppopulation = []
         for i in range(self.popSize):
             for j in range(self.children[i]):
-                temppopulation[index] = self.population[i]
+                temppopulation.append(self.population[i])
                 index += 1
         self.population = temppopulation
 
     def mutation(self):
-        return
+        for x in self.population:
+            numchanges = random.randint(1, 33)
+            for y in range(numchanges):
+                # popsition to change
+                pos = random.randint(0,self.weights-1)
+                # how much to change by
+                change = random.uniform(0, 1)
+                # if 1 add else subtract
+                operator = random.randint(0, 1)
+                if(operator == 1):
+                    x[pos] += change
+                else:
+                    x[pos] -= change
 
     def initPopulattion(self):
         tempweights = []
@@ -66,6 +83,7 @@ class GA:
         self.fitness.clear()
         self.results.clear()
         self.children.clear()
+        self.averageFitness = 0.0
 
     def runGA(self):
         self.initPopulattion()
