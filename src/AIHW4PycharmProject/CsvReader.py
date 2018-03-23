bc806b95965812e5fd6b5316bc219174762539da
davdevor <drdcustomlows@gmail.com>
2018-03-23 11:37:30 -0400

    cahnged readCsv to get attributes I specified

--- src/AIHW4PycharmProject/CsvReader.py
+++ src/AIHW4PycharmProject/CsvReader.py
@@ -1,9 +1,14 @@
 import csv
-
+#the 10 attributes im using studytime, failures,paid,higher,freetime,absences,G1,G2,internet,schoolsup
 def readCsv(fileName):
     file = open(fileName,mode='r',encoding='UTF-8')
     reader = csv.DictReader(file, delimiter=';')
+    attributes = ['studytime','failures','paid','higher','freetime','absences','G1','G2','internet','schoolsup','G3']
     records = []
+    tempDict = {}
     for x in reader:
-        records.append(x)
+        for y in attributes:
+            tempDict[y] = x[y]
+        records.append(tempDict)
+        tempDict = {}
     return records
